1ca30dd9ba61740611110d0f8e40d575eecef287
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-09-12 19:24:40 +0300

    Add project files.

---
+++ lab5/Program.cs
@@ -0,0 +1,36 @@
+﻿using lab5;
+
+CustomTimer timer = new CustomTimer(1000, () =>
+{
+    double x = GetRandomValue(); // getting random x
+    double result = CalculateFunction(x); // calculating function for x
+    Console.WriteLine($"x = {x}, f(x) = {result}");
+});
+
+timer.Start();
+
+Console.WriteLine("Press Enter to stop this thread...");
+Console.ReadLine();
+timer.Stop();
+
+static double GetRandomValue()
+{
+    Random random = new Random();
+    return random.NextDouble() * 10 - 5; // generating x [-5, 5]
+}
+
+static double CalculateFunction(double x)
+{
+    if (-3 <= x && x <= 3)
+    {
+        return 2 / (x * x) + 1;
+    }
+    else
+    {
+        if (x == 0)
+        {
+            throw new ArgumentException("x can't be 0");
+        }
+        return Math.Abs((x * x * x - 7) / x);
+    }
+}
---
+++ lab5/Timer.cs
@@ -0,0 +1,40 @@
+﻿namespace lab5;
+
+class CustomTimer
+{
+    private int interval;
+    private Action callback;
+    private Thread thread;
+    private bool running;
+
+    public CustomTimer(int interval, Action callback)
+    {
+        this.interval = interval;
+        this.callback = callback;
+        thread = new Thread(Run);
+        running = false;
+    }
+
+    public void Start()
+    {
+        if (!running)
+        {
+            running = true;
+            thread.Start();
+        }
+    }
+
+    public void Stop()
+    {
+        running = false;
+    }
+
+    private void Run()
+    {
+        while (running)
+        {
+            Thread.Sleep(interval);
+            callback();
+        }
+    }
+}
